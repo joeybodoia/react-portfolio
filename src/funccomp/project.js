c79f33ea62ad6e214975820a83a60271799e6424
joeybodoia <53023300+joeybodoia@users.noreply.github.com>
2020-10-17 12:36:33 -0700

    project.js

--- src/funccomp/project.js
+++ src/funccomp/project.js
@@ -1,19 +1,18 @@
-import React from "react"
+import React from "react";
 
 const Project = (props) => {
-  
-  return(
+  return (
     <div>
       <h1>{props.name}</h1>
       <h3>
         <a href={props.git}>github page</a>
       </h3>
       <h3>
-        <a href ={props.live}>Live app</a>
+        <a href={props.live}>Live app</a>
       </h3>
-      <img src={props.img} alt={props.alt}/>
+      <img src={props.img} alt={props.alt} />
     </div>
-  )
-}
+  );
+};
 
-export default Project
+export default Project;
